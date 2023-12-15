d20be7de5f6811f261e783f399ee953492a68b8b
Juan-Antonio-Doe <jaespinosag@esimurcia.com>
2023-12-15 20:20:52 +0100

    [CasiFinal] Terminado Pass.
    + Falta:
    - Testeo con gafas.
    - Build.
    - Grabar gameplay.
    - Memoria.

--- Assets/z_Packages/ESI/Juan_Antonio_Espinosa/VR/Scripts/HandControllers/MenuManager.cs
+++ Assets/z_Packages/ESI/Juan_Antonio_Espinosa/VR/Scripts/HandControllers/MenuManager.cs
@@ -7,10 +7,27 @@ public class MenuManager : MonoBehaviour {
 
     [SerializeField] private GameObject handCanvas;
     [SerializeField] private InputActionProperty menuButton;
+    [SerializeField] private InputActionProperty simulatorMenuButton;
 
     void Update() {
-        if (menuButton.action.WasPressedThisFrame()) {
+        if (menuButton.action.WasPressedThisFrame() || simulatorMenuButton.action.WasPressedThisFrame()) {
             handCanvas.SetActive(!handCanvas.activeSelf);
         }
+    }
+
+    public void StartGame() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void Restart() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Exit() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
